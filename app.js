04bd316031876a40a98f5a15edcdaac2f66517aa
Mark Price <pricehome@gmail.com>
2019-11-28 14:39:11 -0600

    items adding to list

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
 // Define Variables
 const form = document.querySelector("#task-form")
 const taskList = document.querySelector(".collection")
-const clearBtn = document.querySelector(".clear-items")
+const clearBtn = document.querySelector(".clear-tasks")
 const filter = document.querySelector("#filter")
 const taskInput = document.querySelector("#task")
+
+// Function to load all event listeners
+loadEventListener()
+
+// Load all eventlisteners
+function loadEventListener() {
+	// Add task event
+	form.addEventListener("submit", addTask)
+}
+
+// Add Task
+function addTask(e) {
+	if (taskInput.value === "") {
+		alert("add an item")
+	}
+
+	// create li element
+	const li = document.createElement("li")
+
+	// add class to li
+	li.className = ".collection-item"
+
+	// create text node and append to li
+	li.appendChild(document.createTextNode(taskInput.value))
+
+	// create new link element
+	const link = document.createElement("a")
+
+	// add class to link
+	link.className = "delete-item secondary-content"
+
+	// add icon HTML
+	link.innerHTML = '<i class= "fa fa-remove"></i>'
+
+	// append link to li
+	li.appendChild(link)
+
+	// append li to ul
+	taskList.appendChild(li)
+
+	// clear input
+	taskInput.value = ""
+
+	e.preventDefault()
+}
